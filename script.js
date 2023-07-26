f27271baefb9ef843a6f2be3dcf35399f000ad54
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-26 14:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,32 +4,40 @@ const tbody= document.getElementById("output");
 
 let prom1=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
-		resolve({name1:"Promise 1", time1:2});
+		 let start=performance.now();
+		resolve({name1:"Promise 1", time1:2,tt:start});
 	},1000)
 })
 let prom2=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
-		resolve({name1:"Promise 2",time1:1});
+		 let start=performance.now();
+		resolve({name1:"Promise 2",time1:1,tt:start});
 	},2000)
 })
 let prom3=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
-		resolve({name1:"Promise 3", time1:3});
+		 let start=performance.now();
+		resolve({name1:"Promise 3", time1:3,tt:start});
 	},3000)
 })
 
 let promises=[prom1,prom2,prom3];
 let x=Promise.all(promises);
 x.then((data)=>{
+	let tr=document.getElementsByClassName("task");
+	let total=0;
   for(let i=0;i<promises.length;i++){
-	  let tr=document.createElement("tr");
+	  
 		let td1=document.createElement("td");
 		td1.innerHTML=data[i].name1;
 	  let td2=document.createElement("td");
-		td2.innerHTML=data[i].time1;
-	  tr.append(td1,td2);
-	  tbody.appendChild(tr);
+		td2.innerHTML=data[i].tt;
+	  tr[i].append(td1,td2);
+	total+=data[i].tt;
     // console.log(data[i].time1);
   }
+	let tot= document.getElementById("total");
+	tot.innerHTML=total;
+	
 })
 
